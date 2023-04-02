7402c72444358d375a22afe911ce318b07844302
luis patino <lcp.patino@gmail.com>
2023-04-02 09:21:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+
+console.log('The company name is:', companyName);
+
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+console.log('The most profitable neighborhood is:', mostProfitableNeighborhood);
+
+
+let companyCeo = 'Susan Smith';
+
+console.log('The CEO of the company is:', companyCeo);
